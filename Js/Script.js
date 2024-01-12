03730aa5fb38d9212a0b428d58d304cc156e7688
Abhijeet2765 <abhijeetdehmukh.2765@gmail.com>
2024-01-12 08:21:38 +0530

    CS003 Worked on disappearing menu in mobile view

--- Js/Script.js
+++ Js/Script.js
@@ -1,29 +1,33 @@
-let menuIcon= document.querySelector('#menu-icon');
-let navbar= document.querySelector('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-menuIcon.onclick=()=>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    navbar.classList.toogle('active');
-}
-/*==========Scroll section active links==========*/
-let sections=document.querySelectorAll('Section');
-let navLinks=document.querySelectorAll('header nav a')
+    navbar.classList.toggle('active');
 
-window.onscroll=()=>{
-    sections.forEach(sec=>{
-        let top=window.scrollY;
-        let offset= sec.offsetTop-150;
-        let height=sec.offsetHeight;
-        let id=sec.getAttribute('id');
+};
 
-        if (top>=offset && top<offset+height){
-            navLinks.forEach(links=>{
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         }
     });
+
     /*==========Sticky navbar==========*/
-    let header= document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY>100);
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
